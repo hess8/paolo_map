189a41ce1ae978561808537b738089d577803292
Bret Hess <bret.hess@gmail.com>
2024-05-19 23:03:39 -0600

    trying to get LeafletGeoAdmin to work

--- paolo_map/admin.py
+++ paolo_map/admin.py
@@ -1,16 +1,8 @@
 from django.contrib.gis import admin
 from .models import Location
-#
-@admin.register(Location)
-class MarkerAdmin(admin.GISModelAdmin):
-    list_display = ("name", "location")
+from leaflet.admin import LeafletGeoAdmin
 
-# from leaflet.admin import LeafletGeoAdmin
-#
-#
-# # @admin.register(Location)
-# # class MarkerAdmin(LeafletGeoAdmin):
-# #     list_display = ("name", "location")
-#
+admin.site.register(Location, admin.GISModelAdmin)
 # admin.site.register(Location, LeafletGeoAdmin)
-# # from django.contrib import admin
+
+
