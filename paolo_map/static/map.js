8693b2af596348406028f982b57a2ef8db3c84d1
Bret Hess <bret.hess@gmail.com>
2024-05-20 12:05:35 -0600

    small changes

--- paolo_map/static/map.js
+++ paolo_map/static/map.js
@@ -2,15 +2,16 @@ const copy =
   "&copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a>";
 
 const url =
-'https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}'
-//  "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
+'http://services.arcgisonline.com/arcgis/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}'
+
 const layer = L.tileLayer(url, {
-  attribution: copy,
-});
+      attribution: copy,
+      });
+
 const map = L.map("map", {
-  layers: [layer],
-  minZoom: 5,
-});
+      layers: [layer],
+      minZoom: 5,
+      });
 
 map
   .locate()
