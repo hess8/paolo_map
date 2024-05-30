8421b037ad46aecc703eea9a5c4edba7cdb4d40b
Bret Hess <bret.hess@gmail.com>
2024-05-29 21:49:32 -0600

    experimenting with urls

--- assets/scripts/map.js
+++ assets/scripts/map.js
@@ -5,13 +5,21 @@ let config = {
     minZoom: 5
 };
 
+const zoom = 18;
+// co-ordinates
+const lat = 52.22977;
+const lng = 21.01178;
+
+// calling map
+
+
 const copy =
   "&copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a>";
 // my code
-const url =
-'http://services.arcgisonline.com/arcgis/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}'
-
-const map = L.map("map", config)
+const url = "https://tile.openstreetmap.org/{z}/{x}/{y}.png";
+const map = L.map("map", config).setView([lat, lng], zoom);
+//const url = 'http://services.arcgisonline.com/arcgis/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}';
+//const map = L.map("map", config)
 
 const tiles = L.tileLayer(url, {
 attribution: copy,
